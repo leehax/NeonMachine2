c5c0e01469aa91e3826f04cec555b0ae9f67b9a2
Tobias W <sersest112@gmail.com>
2018-01-27 22:32:58 +0100

    hey

---
+++ NeonMachine/Assets/MenuTriangle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTriangle : MonoBehaviour {
+
+    [SerializeField]
+    float rotSpeed;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotSpeed * Time.deltaTime);
+    }
+}
