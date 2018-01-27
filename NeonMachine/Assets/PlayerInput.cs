4c6f9861e119c0e18354a926ab7eb5c02e9fe6ad
Leevi Hakala <leevi.hakala@gmail.com>
2018-01-27 11:42:34 +0100

    Unity project

---
+++ NeonMachine/Assets/PlayerInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	// Use this for initialization
+    private Rigidbody2D rigidbody;
+
+    private int playerID;
+
+    private Vector2 direction;
+    private float maxVelocity = 5.0f;
+	void Start ()
+	{
+	    rigidbody = GetComponent<Rigidbody2D>();
+	    playerID = 0;
+        direction = new Vector2(0,0);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    direction.x = Input.GetAxis("HorizontalGamePad" + playerID);
+	    direction.y = Input.GetAxis("VerticalGamePad" + playerID);
+
+	    if (Input.GetButton("Fire1"))
+	    {
+	        rigidbody.AddForce(direction);
+            
+	    }
+	    if (rigidbody.velocity.magnitude > maxVelocity)
+	    {
+
+	        rigidbody.velocity = (rigidbody.velocity / rigidbody.velocity.magnitude) * maxVelocity;
+	    }
+
+	}
+
+}
